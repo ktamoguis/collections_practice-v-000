101f5dffefc6801e4795a9a72bc4de16d7e86e15
Karl Amoguis <ktamoguis@gmail.com>
2018-04-28 07:23:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,5 +18,21 @@ def swap_elements(array)
 end
 
 def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
 
 end
