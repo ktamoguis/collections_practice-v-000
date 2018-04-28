99d703aab75e278698bb2525ddd912e244ae3094
Karl Amoguis <ktamoguis@gmail.com>
2018-04-28 06:52:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  new = array.sort
+  new.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |x,y| x.length <=> y.length }
+end
